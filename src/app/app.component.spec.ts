8dbc983e03ff618753e2a886a8d45c863d1326ea
utamuratov <43562558+utamuratov@users.noreply.github.com>
2023-04-28 10:42:03 +0500

    projectname changed to kiril-lotin

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kril-latin'`, () => {
+  it(`should have as title 'kiril-lotin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kril-latin');
+    expect(app.title).toEqual('kiril-lotin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('kril-latin app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'kiril-lotin app is running!'
+    );
   });
 });
